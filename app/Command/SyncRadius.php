0bc2cb0ea47c9942b7bc773f46a54dd614d2d102
glzjin <glzjin@zhaojin97.cn>
2016-03-31 00:09:39 +0800

    a bug fix

--- app/Command/SyncRadius.php
+++ app/Command/SyncRadius.php
@@ -181,7 +181,7 @@ class SyncRadius
 			echo "Send sync mail to user: ".$user->id;
 			$subject = Config::get('appName')."-密码更新通知";
 			$to = $user->email;
-			$text = "您好，为了保证密码系统的统一，刚刚系统已经将您 vpn 等连接方式的用户名已经重置为：".$email."，密码自动重置为您 ss 的密码：". $user->passwd."  了，以后您修改 ss 密码就会自动修改 vpn 等连接方式的密码了，感谢您的支持。 " ;
+			$text = "您好，为了保证密码系统的统一，刚刚系统已经将您 vpn 等连接方式的用户名已经重置为：".Radius::GetUserName($email)."，密码自动重置为您 ss 的密码：". $user->passwd."  了，以后您修改 ss 密码就会自动修改 vpn 等连接方式的密码了，感谢您的支持。 " ;
 			try{
 				Mail::send($to,$subject,$text);
 			}catch(\Exception $e){
