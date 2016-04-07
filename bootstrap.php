868e5bbfd337121436f9a9d9c5896559ddd3258d
glzjin <glzjin@zhaojin97.cn>
2016-04-07 18:11:12 +0800

    version

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,7 @@ use App\Services\Boot;
 
 //  BASE_PATH
 define('BASE_PATH', __DIR__);
-define('VERSION', '3.2.0 - glzjin mod b1-1604020900');
+define('VERSION', '3.2.0 - glzjin mod b1-1604071800');
 
 // Vendor Autoload
 require BASE_PATH.'/vendor/autoload.php';
