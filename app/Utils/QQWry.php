0e5777669189da5bfbb15d88b00b7727d8ca3140
glzjin <glzjin@zhaojin97.cn>
2016-04-14 09:49:40 +0800

    utf8 unix

--- app/Utils/QQWry.php
+++ app/Utils/QQWry.php
@@ -2,53 +2,20 @@
 namespace App\Utils;
 
 
-/**  
-* IP 地理位置查询类  
-*   
+/*
 * @author joyphper  
-* @version 1.0  
-* @copyright 2010 joyphper.net  
 */ 
  
 class QQWry {  
-     /**  
-      * QQWry.Dat文件指针  
-      *  
-      * @var resource  
-      */ 
  
      private $fp;  
  
-     /**  
-      * 第一条IP记录的偏移地址  
-      *  
-      * @var int  
-      */ 
- 
      private $firstip;  
  
-     /**  
-      * 最后一条IP记录的偏移地址  
-      *  
-      * @var int  
-      */ 
- 
      private $lastip;  
- 
-     /**  
-      * IP记录的总条数（不包含版本信息记录）  
-      *  
-      * @var int  
-      */ 
        
      private $totalip;  
  
-    /**  
-      * 构造函数，打开 QQWry.Dat 文件并初始化类中的信息  
-      *  
-      * @param string $filename  
-      * @return IpLocation  
-      */ 
  
      public function __construct($filename = BASE_PATH."/app/Utils/qqwry.dat") {  
  
@@ -62,19 +29,12 @@ class QQWry {
  
              $this->totalip = ($this->lastip - $this->firstip) / 7;  
  
-             //注册析构函数，使其在程序执行结束时执行  
- 
              register_shutdown_function(array(&$this, '__destruct'));  
                
          }  
  
      }  
- 
-     /**  
- 
-      * 析构函数，用于在页面执行结束后自动关闭打开的文件。  
-      *  
-      */ 
+
  
     public function __destruct() {  
  
@@ -87,18 +47,10 @@ class QQWry {
          $this->fp = 0;  
  
      }  
-       
-     /**  
- 
-      * 返回读取的长整型数  
-      *  
-      * @access private  
-      * @return int  
-      */ 
+      
  
     private function getlong() {  
- 
-         //将读取的little-endian编码的4个字节转化为长整型数  
+  
  
          $result = unpack('Vlong', fread($this->fp, 4));  
  
@@ -106,17 +58,8 @@ class QQWry {
  
      }  
  
-     /**  
- 
-      * 返回读取的3个字节的长整型数  
-      *  
-      * @access private  
-      * @return int  
-      */ 
  
      private function getlong3() {  
- 
-         //将读取的little-endian编码的3个字节转化为长整型数  
  
          $result = unpack('Vlong', fread($this->fp, 3).chr(0));  
  
@@ -124,40 +67,21 @@ class QQWry {
  
      }  
  
-     /**  
-      * 返回压缩后可进行比较的IP地址  
-      *  
-      * @access private  
-      * @param string $ip  
-      * @return string  
-      */ 
  
      private function packip($ip) {  
-          
-         // 将IP地址转化为长整型数，如果在PHP5中，IP地址错误，则返回False，  
-           
-         // 这时intval将Flase转化为整数-1，之后压缩成big-endian编码的字符串  
  
          return pack('N', intval(ip2long($ip)));  
  
      }  
  
-     /**  
- 
-      * 返回读取的字符串  
-      *  
-      * @access private  
-      * @param string $data  
-      * @return string  
-      */ 
  
      private function getstring($data = "") {  
  
          $char = fread($this->fp, 1);  
  
-         while (ord($char) > 0) {        // 字符串按照C格式保存，以结束  
+         while (ord($char) > 0) {       
  
-             $data .= $char;             // 将读取的字符连接到给定字符串之后  
+             $data .= $char;            
  
              $char = fread($this->fp, 1);  
  
@@ -167,20 +91,13 @@ class QQWry {
  
      }  
  
-     /**  
-      * 返回地区信息  
-      *  
-      * @access private  
-      * @return string  
-      */ 
- 
      private function getarea() {  
  
-         $byte = fread($this->fp, 1);    // 标志字节  
+         $byte = fread($this->fp, 1);    
  
          switch (ord($byte)) {  
  
-             case 0:                     // 没有区域信息  
+             case 0:                    
  
                  $area = "";  
  
@@ -188,7 +105,7 @@ class QQWry {
  
              case 1:  
  
-             case 2:                     // 标志字节为1或2，表示区域信息被重定向  
+             case 2:                    
  
                  fseek($this->fp, $this->getlong3());  
  
@@ -196,7 +113,7 @@ class QQWry {
  
                  break;  
  
-             default:                    // 否则，表示区域信息没有被重定向  
+             default:      
  
                  $area = $this->getstring($byte);  
  
@@ -208,47 +125,36 @@ class QQWry {
  
      }  
  
-     /**  
-      * 根据所给 IP 地址或域名返回所在地区信息  
-      *  
-      * @access public  
-      * @param string $ip  
-      * @return array  
-      */ 
  
      public function getlocation($ip) {  
  
-         if (!$this->fp) return null;            // 如果数据文件没有被正确打开，则直接返回空  
+         if (!$this->fp) return null;             
  
-         $location['ip'] = gethostbyname($ip);   // 将输入的域名转化为IP地址  
+         $location['ip'] = gethostbyname($ip);   
+		 
+         $ip = $this->packip($location['ip']);   
  
-         $ip = $this->packip($location['ip']);   // 将输入的IP地址转化为可比较的IP地址  
+                                               
+											   
  
-                                                 // 不合法的IP地址会被转化为255.255.255.255  
  
-         // 对分搜索  
+         $l = 0;                       
  
-         $l = 0;                         // 搜索的下边界  
+         $u = $this->totalip;          
  
-         $u = $this->totalip;            // 搜索的上边界  
+         $findip = $this->lastip;         
  
-         $findip = $this->lastip;        // 如果没有找到就返回最后一条IP记录（QQWry.Dat的版本信息）  
+         while ($l <= $u) {              
  
-         while ($l <= $u) {              // 当上边界小于下边界时，查找失败  
- 
-             $i = floor(($l + $u) / 2); // 计算近似中间记录  
+             $i = floor(($l + $u) / 2); 
  
              fseek($this->fp, $this->firstip + $i * 7);  
  
-            $beginip = strrev(fread($this->fp, 4));     // 获取中间记录的开始IP地址  
+            $beginip = strrev(fread($this->fp, 4));  
  
-             // strrev函数在这里的作用是将little-endian的压缩IP地址转化为big-endian的格式  
+             if ($ip < $beginip) {  
  
-             // 以便用于比较，后面相同。  
- 
-             if ($ip < $beginip) {       // 用户的IP小于中间记录的开始IP地址时  
- 
-                 $u = $i - 1;            // 将搜索的上边界修改为中间记录减一  
+                 $u = $i - 1;     
  
              }  
  
@@ -256,19 +162,19 @@ class QQWry {
  
                  fseek($this->fp, $this->getlong3());  
  
-                 $endip = strrev(fread($this->fp, 4));   // 获取中间记录的结束IP地址  
+                 $endip = strrev(fread($this->fp, 4));   
  
-                 if ($ip > $endip) {     // 用户的IP大于中间记录的结束IP地址时  
+                 if ($ip > $endip) {   
  
-                     $l = $i + 1;        // 将搜索的下边界修改为中间记录加一  
+                     $l = $i + 1;       
  
                  }  
  
-                 else {                  // 用户的IP在中间记录的IP范围内时  
+                 else {                
  
                      $findip = $this->firstip + $i * 7;  
  
-                     break;              // 则表示找到结果，退出循环  
+                     break;            
  
                  }  
  
@@ -276,33 +182,33 @@ class QQWry {
  
          }  
  
-         //获取查找到的IP地理位置信息  
+         
  
          fseek($this->fp, $findip);  
  
-         $location['beginip'] = long2ip($this->getlong());   // 用户IP所在范围的开始地址  
+         $location['beginip'] = long2ip($this->getlong());  
  
          $offset = $this->getlong3();  
  
          fseek($this->fp, $offset);  
  
-         $location['endip'] = long2ip($this->getlong());     // 用户IP所在范围的结束地址  
+         $location['endip'] = long2ip($this->getlong());    
  
-         $byte = fread($this->fp, 1);    // 标志字节  
+         $byte = fread($this->fp, 1);  
  
          switch (ord($byte)) {  
  
-             case 1:                     // 标志字节为1，表示国家和区域信息都被同时重定向  
+             case 1:                     
  
-                 $countryOffset = $this->getlong3();         // 重定向地址  
+                 $countryOffset = $this->getlong3();        
  
                  fseek($this->fp, $countryOffset);  
  
-                 $byte = fread($this->fp, 1);    // 标志字节  
+                 $byte = fread($this->fp, 1);   
  
                  switch (ord($byte)) {  
  
-                     case 2:             // 标志字节为2，表示国家信息又被重定向  
+                     case 2:            
  
                          fseek($this->fp, $this->getlong3());  
  
@@ -314,7 +220,7 @@ class QQWry {
  
                          break;  
  
-                     default:            // 否则，表示国家信息没有被重定向  
+                     default:       
  
                          $location['country'] = $this->getstring($byte);  
  
@@ -326,7 +232,7 @@ class QQWry {
  
                  break;  
  
-             case 2:                     // 标志字节为2，表示国家信息被重定向  
+             case 2:                    
  
                  fseek($this->fp, $this->getlong3());  
  
@@ -338,7 +244,7 @@ class QQWry {
  
                  break;  
  
-             default:                    // 否则，表示国家信息没有被重定向  
+             default:                    
  
                  $location['country'] = $this->getstring($byte);  
  
@@ -348,7 +254,7 @@ class QQWry {
  
          }  
  
-         if ($location['country'] == " CZ88.NET") { // CZ88.NET表示没有有效信息  
+         if ($location['country'] == " CZ88.NET") { 
  
              $location['country'] = "未知";  
  
