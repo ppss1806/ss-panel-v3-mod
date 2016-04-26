c98c913f0e529916a433bae8d90615cb166cfb13
glzjin <glzjin@zhaojin97.cn>
2016-04-27 00:57:11 +0800

    all sql

---
+++ glzjin_all.sql
@@ -0,0 +1,491 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2016-04-27 00:54:16
+-- 服务器版本： 5.5.48-log
+-- PHP Version: 5.6.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `glzjin_ss2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `alive_ip`
+--
+
+CREATE TABLE IF NOT EXISTS `alive_ip` (
+  `id` bigint(20) NOT NULL,
+  `nodeid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `ip` text NOT NULL,
+  `datetime` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `announcement`
+--
+
+CREATE TABLE IF NOT EXISTS `announcement` (
+  `id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `code`
+--
+
+CREATE TABLE IF NOT EXISTS `code` (
+  `id` bigint(20) NOT NULL,
+  `code` text NOT NULL,
+  `type` int(11) NOT NULL,
+  `number` int(11) NOT NULL,
+  `isused` int(11) NOT NULL DEFAULT '0',
+  `userid` bigint(20) NOT NULL,
+  `usedatetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `link`
+--
+
+CREATE TABLE IF NOT EXISTS `link` (
+  `id` bigint(20) NOT NULL,
+  `type` int(11) NOT NULL,
+  `address` text NOT NULL,
+  `port` int(11) NOT NULL,
+  `token` text NOT NULL,
+  `ios` int(11) NOT NULL DEFAULT '0',
+  `userid` bigint(20) NOT NULL,
+  `isp` text,
+  `geo` int(11) DEFAULT NULL,
+  `method` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `login_ip`
+--
+
+CREATE TABLE IF NOT EXISTS `login_ip` (
+  `id` bigint(20) NOT NULL,
+  `userid` bigint(20) NOT NULL,
+  `ip` text NOT NULL,
+  `datetime` bigint(20) NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `radius_ban`
+--
+
+CREATE TABLE IF NOT EXISTS `radius_ban` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `speedtest`
+--
+
+CREATE TABLE IF NOT EXISTS `speedtest` (
+  `id` bigint(20) NOT NULL,
+  `nodeid` int(11) NOT NULL,
+  `datetime` bigint(20) NOT NULL,
+  `telecomping` text NOT NULL,
+  `telecomeupload` text NOT NULL,
+  `telecomedownload` text NOT NULL,
+  `unicomping` text NOT NULL,
+  `unicomupload` text NOT NULL,
+  `unicomdownload` text NOT NULL,
+  `cmccping` text NOT NULL,
+  `cmccupload` text NOT NULL,
+  `cmccdownload` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ss_invite_code`
+--
+
+CREATE TABLE IF NOT EXISTS `ss_invite_code` (
+  `id` int(11) NOT NULL,
+  `code` varchar(128) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ss_node`
+--
+
+CREATE TABLE IF NOT EXISTS `ss_node` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `type` int(3) NOT NULL,
+  `server` varchar(128) NOT NULL,
+  `method` varchar(64) NOT NULL,
+  `info` varchar(128) NOT NULL,
+  `status` varchar(128) NOT NULL,
+  `sort` int(3) NOT NULL,
+  `custom_method` tinyint(1) NOT NULL DEFAULT '0',
+  `traffic_rate` float NOT NULL DEFAULT '1',
+  `node_class` int(11) NOT NULL DEFAULT '0',
+  `node_speedlimit` int(11) NOT NULL DEFAULT '0',
+  `node_connector` int(11) NOT NULL DEFAULT '0',
+  `node_bandwidth` bigint(20) NOT NULL DEFAULT '0',
+  `node_bandwidth_limit` bigint(20) NOT NULL DEFAULT '0',
+  `bandwidthlimit_resetday` int(11) NOT NULL DEFAULT '0',
+  `node_heartbeat` bigint(20) NOT NULL DEFAULT '0',
+  `node_ip` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ss_node_info`
+--
+
+CREATE TABLE IF NOT EXISTS `ss_node_info` (
+  `id` int(11) NOT NULL,
+  `node_id` int(11) NOT NULL,
+  `uptime` float NOT NULL,
+  `load` varchar(32) NOT NULL,
+  `log_time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ss_node_online_log`
+--
+
+CREATE TABLE IF NOT EXISTS `ss_node_online_log` (
+  `id` int(11) NOT NULL,
+  `node_id` int(11) NOT NULL,
+  `online_user` int(11) NOT NULL,
+  `log_time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ss_password_reset`
+--
+
+CREATE TABLE IF NOT EXISTS `ss_password_reset` (
+  `id` int(11) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `init_time` int(11) NOT NULL,
+  `expire_time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ss_user_admin`
+--
+
+CREATE TABLE IF NOT EXISTS `ss_user_admin` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `pass` varchar(64) NOT NULL,
+  `passwd` varchar(16) NOT NULL,
+  `t` int(11) NOT NULL DEFAULT '0',
+  `u` bigint(20) NOT NULL,
+  `d` bigint(20) NOT NULL,
+  `plan` varchar(2) CHARACTER SET utf8mb4 NOT NULL DEFAULT 'A',
+  `transfer_enable` bigint(20) NOT NULL,
+  `port` int(11) NOT NULL,
+  `switch` tinyint(4) NOT NULL DEFAULT '1',
+  `enable` tinyint(4) NOT NULL DEFAULT '1',
+  `type` tinyint(4) NOT NULL DEFAULT '1',
+  `last_get_gift_time` int(11) NOT NULL DEFAULT '0',
+  `last_check_in_time` int(11) NOT NULL DEFAULT '0',
+  `last_rest_pass_time` int(11) NOT NULL DEFAULT '0',
+  `reg_date` datetime NOT NULL,
+  `invite_num` int(8) NOT NULL,
+  `money` decimal(12,2) NOT NULL,
+  `ref_by` int(11) NOT NULL DEFAULT '0',
+  `expire_time` int(11) NOT NULL DEFAULT '0',
+  `method` varchar(64) NOT NULL DEFAULT 'rc4-md5',
+  `is_email_verify` tinyint(4) NOT NULL DEFAULT '0',
+  `reg_ip` varchar(128) NOT NULL DEFAULT '127.0.0.1',
+  `node_speedlimit` text NOT NULL,
+  `node_connector` int(11) NOT NULL DEFAULT '0',
+  `is_admin` int(2) NOT NULL DEFAULT '0',
+  `wechat` text NOT NULL,
+  `last_day_t` bigint(20) NOT NULL DEFAULT '0',
+  `sendDailyMail` int(11) NOT NULL DEFAULT '1',
+  `class` int(11) NOT NULL DEFAULT '0',
+  `class_expire` datetime NOT NULL DEFAULT '1989-06-04 00:05:00',
+  `expire_in` datetime NOT NULL DEFAULT '2099-06-04 00:05:00',
+  `theme` text NOT NULL,
+  `ga_token` text NOT NULL,
+  `ga_enable` int(11) NOT NULL DEFAULT '0',
+  `pac` longtext
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user_token`
+--
+
+CREATE TABLE IF NOT EXISTS `user_token` (
+  `id` int(11) NOT NULL,
+  `token` varchar(256) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `create_time` int(11) NOT NULL,
+  `expire_time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user_traffic_log`
+--
+
+CREATE TABLE IF NOT EXISTS `user_traffic_log` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `u` int(11) NOT NULL,
+  `d` int(11) NOT NULL,
+  `node_id` int(11) NOT NULL,
+  `rate` float NOT NULL,
+  `traffic` varchar(32) NOT NULL,
+  `log_time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alive_ip`
+--
+ALTER TABLE `alive_ip`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `announcement`
+--
+ALTER TABLE `announcement`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `code`
+--
+ALTER TABLE `code`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `link`
+--
+ALTER TABLE `link`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login_ip`
+--
+ALTER TABLE `login_ip`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `radius_ban`
+--
+ALTER TABLE `radius_ban`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `speedtest`
+--
+ALTER TABLE `speedtest`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ss_invite_code`
+--
+ALTER TABLE `ss_invite_code`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `ss_node`
+--
+ALTER TABLE `ss_node`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ss_node_info`
+--
+ALTER TABLE `ss_node_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ss_node_online_log`
+--
+ALTER TABLE `ss_node_online_log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ss_password_reset`
+--
+ALTER TABLE `ss_password_reset`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ss_user_admin`
+--
+ALTER TABLE `ss_user_admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_name` (`user_name`),
+  ADD KEY `uid` (`id`),
+  ADD KEY `email` (`email`);
+
+--
+-- Indexes for table `user_token`
+--
+ALTER TABLE `user_token`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_traffic_log`
+--
+ALTER TABLE `user_traffic_log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alive_ip`
+--
+ALTER TABLE `alive_ip`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `announcement`
+--
+ALTER TABLE `announcement`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `code`
+--
+ALTER TABLE `code`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `link`
+--
+ALTER TABLE `link`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `login_ip`
+--
+ALTER TABLE `login_ip`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `radius_ban`
+--
+ALTER TABLE `radius_ban`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `speedtest`
+--
+ALTER TABLE `speedtest`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ss_invite_code`
+--
+ALTER TABLE `ss_invite_code`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ss_node`
+--
+ALTER TABLE `ss_node`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ss_node_info`
+--
+ALTER TABLE `ss_node_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ss_node_online_log`
+--
+ALTER TABLE `ss_node_online_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ss_password_reset`
+--
+ALTER TABLE `ss_password_reset`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ss_user_admin`
+--
+ALTER TABLE `ss_user_admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_token`
+--
+ALTER TABLE `user_token`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_traffic_log`
+--
+ALTER TABLE `user_traffic_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
