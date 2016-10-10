db39efb3f3a0d90ff7720905202b9c3478525ec1
glzjin <glzjin@zhaojin97.cn>
2016-10-10 08:48:42 +0800

    bump version

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,7 @@ use App\Services\Boot;
 
 //  BASE_PATH
 define('BASE_PATH', __DIR__);
-define('VERSION', '201610092130-v2 final');
+define('VERSION', '201610100900-v2 final');
 
 // Vendor Autoload
 require BASE_PATH.'/vendor/autoload.php';
