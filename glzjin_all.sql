5287b1d39cb3641ae43f5da146757d3df9b366d5
glzjin <glzjin@zhaojin97.cn>
2016-12-25 16:42:14 +0800

    update sql

--- glzjin_all.sql
+++ glzjin_all.sql
@@ -659,25 +659,8 @@ CREATE TABLE IF NOT EXISTS `relay` (
   `priority` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `relay`
---
 ALTER TABLE `relay`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `relay`
---
 ALTER TABLE `relay`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
